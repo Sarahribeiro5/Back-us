4716b825cc562f24f0007d311873c223ea517d66
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2024-11-21 11:32:29 -0300

    feat: create server and indexRoutes files

--- segundaetapa/src/routes/index.routes.js
+++ segundaetapa/src/routes/index.routes.js
@@ -1,17 +1,20 @@
 // Importar a funcionalidade Router do framework Express, utilizado no Node.js
+
 import { Router } from "express";
 
 // Lista de importação das rotas do projeto
-import usuariosRoutes from "./usuarios.routes.js";
+// Não existe rotas no projeto a serem importadas
 
+//Serve para criar uma nova instância do roteador (Router) do Express, que você pode usar para definir e gerenciar diferentes rotas na sua aplicação.
 const routes = Router();
 
-// Rota raiz para teste
+//Serve para definir uma rota GET no módulo de rotas que responde com uma mensagem JSON.
 routes.get("/", (req, res) => {
-  return res.status(200).json({ message: "Vai Corinthians!" });
+    return res.status(200).json({ message: "Vai Corinthians!" });
 });
 
 // Lista de uso das rotas do projeto
-routes.use("/usuarios", usuariosRoutes);
+// Não existem rotas 
 
+//Serve para exportar a constante routes como o valor padrão do módulo. Isso significa que, ao importar esse módulo em outro arquivo, você estará importando a constante routes.
 export default routes;
--- segundaetapa/src/server.js
+++ segundaetapa/src/server.js
@@ -4,17 +4,23 @@ import express from "express";
 // Serve para importar a função config do pacote dotenv.
 import { config } from "dotenv";
 
-// Serve para 
+// Serve para importar as rotas definidas no arquivo index.routes.js que está localizado no diretório routes.
 import routes from "./routes/index.routes.js";
 
+// Serve para executar a função config do módulo dotenv.
 config();
 
-const serverPort = process.env.PORT || 3000;
+// Serve para definir a porta na qual o servidor da aplicação irá escutar.
+const serverPort = process.env.PORT || 4000;
 
+// Serve para criar uma instância da aplicação Express.
 const app = express();
+// Serve para configurar middleware no Express para que ele consiga lidar com requisições HTTP que contenham dados no formato JSON.
 app.use(express.json());
+// Serve para registrar middleware de rotas na aplicação Express.
 app.use(routes);
 
+// Serve para iniciar o servidor da aplicação Express e fazer com que ele comece a escutar na porta especificada por serverPort.
 app.listen(serverPort, () => {
   console.log(`⚡ Server started on http://localhost:${serverPort}`);
 });
